783ea05cdff3c084068fac64f78fc8e9e0b3c322
Frans <malarisco85@gmail.com>
2025-08-04 00:15:29 +0200

    Program Counter

---
+++ index.js
@@ -0,0 +1,56 @@
+//Output some code.
+//console.log("Hello World!");
+
+//window alert
+//window.alert("Are you alive?");
+
+/* This
+    is 
+    a multiline
+    commment
+*/
+
+//Basic DOM manipulation. The getElementById() in js grants you the ability to be able to modify js content anyhow.
+
+//document.getElementById("myHi").textContent = "Hello";
+//document.getElementById("myP").textContent = "Hello World!"
+
+/* Variables:
+        const -> Constant
+        let -> Local variable
+        var -> Apparently you should avoid this as much as possible.
+*/
+
+//Accepting User Input In Java.
+
+//Easy way -> Window Prompt
+/*let username;
+username = window.prompt("What is your username: ")
+console.log(username);
+*/
+
+//Professional Way -> HTML Textbox.
+//let username;
+
+//This is another DOM manipulation where we create a function when an element in the html file is clicked,
+//whatever is included in that function will be called.
+/*
+document.getElementById("MySubmit").onclick = function(){
+    username = document.getElementById("mytext").value;
+    message = document.getElementById("MyH1").textContent = `What's Up ${username}`;
+
+};
+*/
+
+/*
+//Type Casting
+
+//Converting age to an input
+const age = Number(age);
+
+//Converting username to a string
+let username = String(username)
+
+//Converting isStudent to a bool.
+let isStudent = Boolean(isStudent)
+*/
